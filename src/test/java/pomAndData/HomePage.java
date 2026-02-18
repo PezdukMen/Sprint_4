73047dda40930e77f5c2b7817aa5e2428fc9deb1
PezdukMen <MaksBerezik@yandex.ru>
2026-02-18 17:29:17 +0700

    1.0

---
+++ src/test/java/pomAndData/HomePage.java
@@ -0,0 +1,130 @@
+package pomAndData;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HomePage {
+
+    // объявление драйвера
+    private WebDriver driver;
+
+    // Локатор заголовка (но он <div>) "Вопросы о важном"
+    private By questions = By.className("Home_SubHeader__zwi_E");
+
+    // Локаторы выпадающего списка - Заголовок
+    private By accordionItemHeading1 = By.xpath(".//*[@class='accordion__item'][1]");
+    private By accordionItemHeading2 = By.xpath(".//*[@class='accordion__item'][2]");
+    private By accordionItemHeading3 = By.xpath(".//*[@class='accordion__item'][3]");
+    private By accordionItemHeading4 = By.xpath(".//*[@class='accordion__item'][4]");
+    private By accordionItemHeading5 = By.xpath(".//*[@class='accordion__item'][5]");
+    private By accordionItemHeading6 = By.xpath(".//*[@class='accordion__item'][6]");
+    private By accordionItemHeading7 = By.xpath(".//*[@class='accordion__item'][7]");
+    private By accordionItemHeading8 = By.xpath(".//*[@class='accordion__item'][8]");
+    // Локаторы выпадающего списка - Раскрывающаяся панель с текстом
+    private By accordionItemPanel1 = By.cssSelector("#accordion__panel-0");
+    private By accordionItemPanel2 = By.cssSelector("#accordion__panel-1");
+    private By accordionItemPanel3 = By.cssSelector("#accordion__panel-2");
+    private By accordionItemPanel4 = By.cssSelector("#accordion__panel-3");
+    private By accordionItemPanel5 = By.cssSelector("#accordion__panel-4");
+    private By accordionItemPanel6 = By.cssSelector("#accordion__panel-5");
+    private By accordionItemPanel7 = By.cssSelector("#accordion__panel-6");
+    private By accordionItemPanel8 = By.cssSelector("#accordion__panel-7");
+
+    //Локатор кнопки "Да все привыкли" - принятие куки
+    private By cookie = By.id("rcc-confirm-button");
+    //Локатор кнопки "Заказать" - вверху
+    private By buttonUp = By.cssSelector("button.Button_Button__ra12g:not(.Button_Middle__1CSJM)");
+    //Локатор кнопки "Заказать" - внизу
+    private By buttonDown = By.cssSelector("button.Button_Button__ra12g.Button_Middle__1CSJM");
+
+    // Конструктор класс
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Метод скролл до заголовка (но он <div>) "Вопросы о важном"
+    public void scrollQuestions() {
+        WebElement element = driver.findElement(questions);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+    }
+    // Метод скролл до кнопки "Заказать" (внизу)
+    public void scrollButtonDown() { // ожидание, потому что просто скролл иногда падает как минимум в FF
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.elementToBeClickable(buttonDown));
+
+        WebElement element = driver.findElement(buttonDown);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    // Метод принятие куки
+    public void clickCookie() {
+        driver.findElement(cookie).click();
+    }
+
+    // Методы клика по заголовку
+    public void clickAccordionItemHeading1() {
+        driver.findElement(accordionItemHeading1).click();
+    }
+    public void clickAccordionItemHeading2() {
+        driver.findElement(accordionItemHeading2).click();
+    }
+    public void clickAccordionItemHeading3() {
+        driver.findElement(accordionItemHeading3).click();
+    }
+    public void clickAccordionItemHeading4() {
+        driver.findElement(accordionItemHeading4).click();
+    }
+    public void clickAccordionItemHeading5() {
+        driver.findElement(accordionItemHeading5).click();
+    }
+    public void clickAccordionItemHeading6() {
+        driver.findElement(accordionItemHeading6).click();
+    }
+    public void clickAccordionItemHeading7() {
+        driver.findElement(accordionItemHeading7).click();
+    }
+    public void clickAccordionItemHeading8() {
+        driver.findElement(accordionItemHeading8).click();
+    }
+
+    // Методы получения текста раскрывающейся панели
+    public String checkTextAccordionItemPanel1() {
+        return driver.findElement(accordionItemPanel1).getText();
+    }
+    public String checkTextAccordionItemPanel2() {
+        return driver.findElement(accordionItemPanel2).getText();
+    }
+    public String checkTextAccordionItemPanel3() {
+        return driver.findElement(accordionItemPanel3).getText();
+    }
+    public String checkTextAccordionItemPanel4() {
+        return driver.findElement(accordionItemPanel4).getText();
+    }
+    public String checkTextAccordionItemPanel5() {
+        return driver.findElement(accordionItemPanel5).getText();
+    }
+    public String checkTextAccordionItemPanel6() {
+        return driver.findElement(accordionItemPanel6).getText();
+    }
+    public String checkTextAccordionItemPanel7() {
+        return driver.findElement(accordionItemPanel7).getText();
+    }
+    public String checkTextAccordionItemPanel8() {
+        return driver.findElement(accordionItemPanel8).getText();
+    }
+
+    // Методы клика на кнопки "Заказать" (вверху/внизу)
+    public void clickButtonUp() {
+        driver.findElement(buttonUp).click();
+    }
+    public void clickButtonDown() {
+        driver.findElement(buttonDown).click();
+    }
+
+}
---
+++ src/test/java/tests/OrderAScooter.java
@@ -0,0 +1,124 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import pomAndData.HomePage;
+import pomAndData.OrderPage;
+
+@RunWith(Parameterized.class)
+public class OrderAScooter {
+
+    private WebDriver driver;
+    private HomePage objHomePage;
+    private OrderPage objOrderPage;
+
+    // стр1
+    private final String nameUser;
+    private final String surnameUser;
+    private final String addressUser;
+    private final String metroStationUser;
+    private final String telephoneNumbersUser;
+    // стр2
+    private final String dateUser;
+    private final String daysPeriodUser;
+    private final String colorUser;
+    private final String commentUser;
+
+    public OrderAScooter
+            (String nameText, String surnameText, String addressText, String metroStationText, String telephoneNumbers,
+             String dateText, String daysPeriodUser, String color, String commentText) {
+        this.nameUser = nameText;
+        this.surnameUser = surnameText;
+        this.addressUser = addressText;
+        this.metroStationUser = metroStationText;
+        this.telephoneNumbersUser = telephoneNumbers;
+        this.dateUser = dateText;
+        this.daysPeriodUser = daysPeriodUser;
+        this.colorUser = color;
+        this.commentUser = commentText;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getFullOrder() {
+        return new Object[][]{
+                {"Иван", "Иванович", "Советская, 76, 198", "Черкизовская", "89888888881",
+                        "10.10.2026", "сутки", "чёрный жемчуг", "можно не привозить"},
+                /* {"Игорь", "Попов", "Тверская дом 455", "Сокольники", "952335489655",
+                        "29/06/2026", "четверо суток", "серая безысходность", "обязательно необходимо привезти"}, */
+        };
+    }
+
+    @Before
+    public void startBrowser() {
+        driver = new ChromeDriver();
+        // driver = new FirefoxDriver();
+        driver.manage().window().maximize(); // полное открытие окна, что бы не было перекрытия png самоката
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        objHomePage = new HomePage(driver);
+        objHomePage.clickCookie(); // иначе кнопка принятия куки перекрывает
+
+    }
+    // /*
+    @Test // через верхнюю кнопку "Заказать"
+    public void shouldOrderScooterViaTopButton() {
+        objHomePage.clickButtonUp();
+
+        objOrderPage = new OrderPage(driver);
+        // стр1
+        objOrderPage.sendKeysName(nameUser);
+        objOrderPage.sendKeysSurname(surnameUser);
+        objOrderPage.sendKeysAddress(addressUser);
+        objOrderPage.sendKeysMetroStation(metroStationUser);
+        objOrderPage.sendKeysTelephone(telephoneNumbersUser);
+        objOrderPage.clickOnward();
+        // стр2
+        objOrderPage.choiceRentalPeriod(daysPeriodUser); // иначе календарь перекрывает
+        objOrderPage.sendKeysDate(dateUser);
+        objOrderPage.choiceColor(colorUser);
+        objOrderPage.sendKeysComment(commentUser);
+        objOrderPage.clickOrder();
+        objOrderPage.clickOrderConfirmation();
+        // проверка модалки
+        objOrderPage.checkOrderModal();
+    }
+    // */
+    // /*
+    @Test // через нижнюю кнопку "Заказать"
+    public void shouldOrderScooterViaBottomButton() {
+        objHomePage.scrollButtonDown();
+        objHomePage.clickButtonDown();
+
+        objOrderPage = new OrderPage(driver);
+        // стр1
+        objOrderPage.sendKeysName(nameUser);
+        objOrderPage.sendKeysSurname(surnameUser);
+        objOrderPage.sendKeysAddress(addressUser);
+        objOrderPage.sendKeysMetroStation(metroStationUser);
+        objOrderPage.sendKeysTelephone(telephoneNumbersUser);
+        objOrderPage.clickOnward();
+        // стр2
+        objOrderPage.choiceRentalPeriod(daysPeriodUser); // иначе календарь перекрывает
+        objOrderPage.sendKeysDate(dateUser);
+        objOrderPage.choiceColor(colorUser);
+        objOrderPage.sendKeysComment(commentUser);
+        objOrderPage.clickOrder();
+        objOrderPage.clickOrderConfirmation();
+        // проверка модалки
+        objOrderPage.checkOrderModal();
+    }
+    // */
+
+    @After
+    public void offBrowser() {
+        driver.quit();
+    }
+
+}
